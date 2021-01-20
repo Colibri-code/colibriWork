91a9c5bed0529ac9155e453c18727db3e4a911d3
Roberto Hodgson <rhodgson@Robertos-MacBook-Pro.local>
2021-01-20 01:29:45 -0600

    add Company routes

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -10,5 +10,24 @@
 
 module.exports.routes = {
 
+    //ProjectManager Routes
+
+    'POST /ProjectManager/Create': { controller: "ProjectManagerController", action: "create" },
+    'GET /ProjectManager/Read/:id': { controller: "ProjectManagerController", action: "read" },
+    'PATCH /ProjectManager/Update/:id': { controller: "ProjectManagerController", action: "update" },
+    'DELETE /ProjectManager/Delete': { controller: "ProjectManagerController", action: "delete" },    
+
+    //User Routers
+    'POST /user/create':{controller:'UserController', action:'create', cors: {
+    allowOrigins: ['http://localhost:1337'],
+    allowCredentials: false
+    }},
+
+    //Company Routes
+    
+    'POST /company/Create': { controller: "CompanyController", action: "create" },
+    'GET /company/Read/:id': { controller: "CompanyController", action: "read" },
+    'PATCH /company/Update/:id': { controller: "CompanyController", action: "update" },
+    'DELETE /company/Delete': { controller: "CompanyController", action: "delete" },
 
 };
