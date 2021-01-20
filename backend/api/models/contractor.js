30a5a90c4243fe50ed3ddee6ac04c10472215f72
Roberto Hodgson <rhodgson@Robertos-MacBook-Pro.local>
2021-01-20 15:02:09 -0600

    partials progress model contractor

--- backend/api/models/contractor.js
+++ backend/api/models/contractor.js
@@ -7,23 +7,53 @@
 
 module.exports = {
 
+  datastore: 'default',
+  identity: 'contractor',
+  tableName: 'contractors',
+
   attributes: {
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    billingMethod: {
+      type: 'string',
+      columnName: 'billingMethod',
+      required: false,
+      columnType: 'varchar(20)'
+    },
 
+    notificacionSetting: {
+      type: 'string',
+      columnName: 'notificationSetting',
+      required: false,
+      columnType: 'varchar(10)'
+    },
+  
+    social: {
+      type: 'string',
+      columnName: 'social',
+      required: false,
+      columnType: 'varchar(15)'
+    },
+/*
+    //Relationships
+    users: {
+      //an contractor can have multiple users but a user will only have one contractor to belong
+      collection: "user",
+      via: "contractor",
+    },
 
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    companies: {
+      //an contractor can have multiple companies but a companies will only have one contractor to belong
+      collection: "company",
+      via: "contractor",
+    },
 
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
-  },
+    supports: {
+      //an contractor can have multiple companies but a companies will only have one contractor to belong
+      collection: "support",
+      via: "contractor",
+    },
+*/
+  }
 
 };
 
