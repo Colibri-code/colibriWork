c84920d3f4d9af0a441884fd56140d015b6b5a3d
Roberto Hodgson <rhodgson@Robertos-MacBook-Pro.local>
2021-01-20 11:16:13 -0600

    property email modify. Add Relationships to company model

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -1,28 +1,20 @@
-module.export = {
-  datastore: "default",
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  datastore: 'default',
+  identity: 'user',
+  tableName: 'users',
+
   /**Fields BD */
   attributes: {
 
-    email: {
-
-      type: 'string',
-      columnName: 'id',
-      columType: 'int',
-      autoIncrement: true,
-      unique: true,
-      require: true
-    },
-
-    password: {
-
-      type: 'string',
-      columnName: "password",
-      columnType: "varchar(200)",
-      encrypt: true
-    },
-
     firstName: {
-
       type: "string",
       required: false,
       columnName: "firstName",
@@ -34,6 +26,22 @@ module.export = {
       required: false,
       columnName: "secondName",
       columnType: "varchar(45)"
+    },
+
+    password: {
+      type: 'string',
+      columnName: "password",
+      columnType: "varchar(200)",
+      encrypt: true
+    },
+
+    email: {
+      type: "string",
+      columnName: "userEmail",
+      columnType: "varchar(50)",
+      required: true,
+      unique: true,
+      isEmail: true
     },
 
     picture: {
@@ -56,32 +64,40 @@ module.export = {
       columnName: "profile",
       columnType: "varchar(200)"
     },
+    
     location: {
       type: "string",
       required: false,
       columnName: "location",
       columnType: "varchar(20)",
     },
+    
     phoneNumber: {
       type: "string",
       columnName: "phoneNumber",
       columnType: "varchar(20)",
       required: false,
     },
+    
     message: {
       type: "string",
       columnName: "message",
       columnType: "varchar(150)",
       required: false,
     },
+    
     alerts: {
       type: "string",
       columnName: "alerts",
       columnType: "varchar(150)",
       required: false,
-    }
+    },
+    
+    //Relationships
 
-
-
-  },
+    company: {
+      //user will only have one company to belong
+      model: "company",
+    },
+  }
 };
