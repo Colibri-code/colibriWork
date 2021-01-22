72002417c88f211ad0098c3526dd5b92bd41b301
jossuecenteno <77511402+jossuecenteno@users.noreply.github.com>
2021-01-22 12:40:38 -0600

    ReqOri
    routes.js

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -10,16 +10,6 @@
 
 module.exports.routes = {
     
-    'POST /company/Create': { controller: "CompanyController", action: "create" },
-    'GET /company/Read/:id': { controller: "CompanyController", action: "read" },
-    'PATCH /company/Update/:id': { controller: "CompanyController", action: "update" },
-    'DELETE /company/Delete': { controller: "CompanyController", action: "delete" },
-
-    'POST /ProjectManager/Create': { controller: "ProjectManagerController", action: "create" },
-    'GET /ProjectManager/Read/:id': { controller: "ProjectManagerController", action: "read" },
-    'PATCH /ProjectManager/Update/:id': { controller: "ProjectManagerController", action: "update" },
-    'DELETE /ProjectManager/Delete': { controller: "ProjectManagerController", action: "delete" }, 
-
     'GET /api/controllers':'OrganizationController.get',
     'POST /api/controllers': 'OrganizationController.create',
     'PUT /api/controllers/:id': 'OrganizationController.update',
