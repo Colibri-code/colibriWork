1a263ab59c51982b2a28e24f3cde4955e1254070
antonio <antoniosalas1996@gmail.com>
2021-01-27 15:18:52 -0600

    Estructura de carpetas FrontEnd
    Se crea la estructura del FrontEnd. rutas para axios y services.js

---
+++ frontend/src/services.js
@@ -0,0 +1,26 @@
+import http from "./http_common";
+
+
+const get = id => {
+  return http.get(`/user/read/${id}`);
+};
+
+const create = data => {
+  return http.post("/user/create", data);
+};
+
+const update = (id, data) => {
+  return http.path(`/user/update/${id}`, data);
+};
+
+const remove = id => {
+  return http.delete(`/user/delete/${id}`);
+};
+
+export default {
+  get,
+  create,
+  update,
+  remove,
+  
+};
